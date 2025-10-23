51ea563d7d101e3bcc1b70ead34dc9a72b143816
Alex YONDO <ayondo.pm@gmail.com>
2025-10-23 20:57:15 +0200

    Create version.php

---
+++ config/version.php
@@ -0,0 +1,61 @@
+<?php
+return array (
+  'PRODUCT' => 'Versioning',
+  'MAJOR_VERSION' => 0,
+  'MINOR_VERSION' => 0,
+  'PATCH_VERSION' => 2,
+  'EXTRA_VERSION' => 'dev',
+  'DEV_STATUS' => 'Development',
+  'CODENAME' => 'Phoenix',
+  'RELDATE' => '23/10/2025',
+  'COPYRIGHT' => 'Copyright © 2025',
+  'changelog' => 
+  array (
+    '0.2.0' => 
+    array (
+      'release_date' => '23/10/2025',
+      'description' => 'Correction erreur d\'affichage',
+      'added' => 
+      array (
+      ),
+      'changed' => 
+      array (
+      ),
+      'deprecated' => 
+      array (
+      ),
+      'removed' => 
+      array (
+      ),
+      'fixed' => 
+      array (
+      ),
+      'security' => 
+      array (
+      ),
+    ),
+    '0.0.2-dev' => 
+    array (
+      'release_date' => '23/10/2025',
+      'description' => 'Mise a jour retour non initialisé',
+      'added' => 
+      array (
+      ),
+      'changed' => 
+      array (
+      ),
+      'deprecated' => 
+      array (
+      ),
+      'removed' => 
+      array (
+      ),
+      'fixed' => 
+      array (
+      ),
+      'security' => 
+      array (
+      ),
+    ),
+  ),
+);
